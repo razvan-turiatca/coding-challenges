cfb438e8a574cca31647b9c850c5c0e278d91076
razvan-turiatca <codingwithrazvan@gmail.com>
2022-12-24 18:52:17 +0200

    solutions and readme file

---
+++ Strings/The Decimator/index.js
@@ -0,0 +1,14 @@
+// my solutions
+
+function DECIMATOR(str) {
+  return str
+    .split('')
+    .reverse()
+    .slice(Math.ceil(str.length / 10))
+    .reverse()
+    .join('')
+}
+
+function DECIMATOR(str) {
+  return str.slice(0, str.length - Math.ceil(str.length / 10))
+}
